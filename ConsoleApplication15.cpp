b079ae6d73fd1a0b8be6968989faaae795e21b47
injoon149 <dlswns8679@naver.com>
2022-07-23 23:15:30 +0900

    초기 커밋.

---
+++ ConsoleApplication15.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+using namespace std;
+
+int gcd(int a, int b)
+{
+	int m;
+	while (b != 0)
+	{
+		m = a % b;
+		a = b;
+		b = m;
+	}
+	return a;
+}
+
+int main()
+{
+	cin.tie(NULL);
+
+	cout.tie(NULL);
+
+	ios::sync_with_stdio(false);
+	int a, b;
+	cin >> a >> b;
+
+	cout << gcd(a, b) << '\n';
+	cout << a * b / gcd(a, b) << '\n';
+
+	return 0;
+
+
+
+
+}
